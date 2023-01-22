012f9332d2647ac7ba335669aec85accbb146eb1
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-01-22 12:53:50 +0100

    Set up books route

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+app.use('/books', require(path.join(__dirname, 'routes', 'books')));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
