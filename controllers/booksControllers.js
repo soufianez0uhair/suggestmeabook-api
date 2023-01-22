e32ccf9fd2d87274c5c28a04fba8c13c3f3ac0d5
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-01-22 15:13:12 +0100

    Add get suggested books details & complete getBooks route

--- controllers/booksControllers.js
+++ controllers/booksControllers.js
@@ -1,14 +1,25 @@
 const {booksSuggestionTemp} = require('../helpers/promptTemplates');
 const openaiHelper = require('../helpers/openaiHelper');
+const { getBook } = require('../helpers/getBook');
 
 const getBooks = async (req, res) => {
     const prompt = booksSuggestionTemp(req.body.input);
 
     const completion = await openaiHelper(prompt);
 
-    const booksNames = completion.data.choices[0].text;
-
+    const booksNames = completion.data.choices[0].text.split(',');
     
+    const books = new Array(6);
+    
+    for(let i = 0; i < booksNames.length; i++) {
+        books[i] = await getBook(booksNames[i]);
+    }
+
+    return res
+              .status(200)
+              .json({
+                books
+              })
 };
 
 module.exports = getBooks;
---
+++ helpers/getBook.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+exports.getBook = async (input) => {
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${input}`);
+
+    const book = response.data.items[0];
+
+    return {
+        title: `${book.volumeInfo.title}${book.volumeInfo.subtitle ? `: ${book.volumeInfo.subtitle}` : ''}`,
+        authors: book.volumeInfo.authors,
+        frontCover: book.volumeInfo.imageLinks?.thumbnail,
+        description: book.volumeInfo.description,
+        publisher: book.volumeInfo.publisher
+    }
+}
